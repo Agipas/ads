9fa97a271bfbf45fc2f8d343ef7047f88f8d149d
Volodymyr Holubets <vholubets@n-ix.com>
2015-10-13 12:41:36 +0300

    fix

--- discnt/discnt.py
+++ discnt/discnt.py
@@ -35,14 +35,13 @@ def main():
     input_file = 'discnt.in'
     output_file = 'discnt.out'
     prices, disc = read_file(input_file)
-    print prices, disc
     sorting(prices)
     actual_disc_num = len(prices) / allowed_disc_num
     if actual_disc_num > 0:
         res = sum(prices[:-actual_disc_num]) + sum(
             [i * (100 - disc)/100. for i in prices[-actual_disc_num:]])
     else:
-        res = sum([prices])
+        res = sum(prices)
     write_file(output_file, "{0:.2f}".format(res))
 
 
