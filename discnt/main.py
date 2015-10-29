18b79945b033293aec69b127eabcce3831d738a7
vwvolodya <vwvolodya@gmail.com>
2015-10-29 23:29:37 +0200

    merge sort draft

--- discnt/main.py
+++ discnt/main.py
@@ -19,6 +19,7 @@ def write_file(path, data):
 
 
 def swap(arr, i, j):
+    # not pythonic way to swap
     arr[i] = arr[i] ^ arr[j]
     arr[j] = arr[i] ^ arr[j]
     arr[i] = arr[i] ^ arr[j]
@@ -28,14 +29,47 @@ def compare(a, b):
     return a > b
 
 
-def _merge(arr1, arr2):
-    result = array('i')
-    i = 0
-    j = 0
-    while i < len(arr1):
-        pass
+def _merge(arr, results, left_begin, right_begin, right_end):
+    left_end = right_begin - 1
+    left_read_pos = left_begin
+    right_read_pos = right_begin
+    result_write_pos = left_begin
+    while left_read_pos <= left_end and right_read_pos <= right_end:
+        if compare(arr[left_read_pos], arr[right_read_pos]):
+            results[result_write_pos] = arr[left_read_pos]
+            left_read_pos += 1
+        else:
+            results[result_write_pos] = arr[right_read_pos]
+            right_read_pos += 1
+        result_write_pos += 1
+    while left_read_pos <= left_end:
+        results[result_write_pos] = arr[left_read_pos]
+        result_write_pos += 1
+        left_read_pos += 1
+    while right_read_pos <= right_end:
+        results[result_write_pos] = arr[left_read_pos]
+        left_read_pos += 1
+        result_write_pos += 1
+    i = left_begin
+    while i < right_end:
+        arr[i] = results[i]
+        i += 1
 
 
-def merge_sort_bottom_up():
-    tmp = array('i', [])
+def sort(arr):
+    size = 1
+    length = len(arr)
+    result = array('i', [0] * length)  # allocate memory
+    while size < length:
+        i = 0
+        while i < length:
+            _merge(arr, result, i, i + size, i + 2 * size)
+            i += 2 * size
+        size *= 2
 
+
+if __name__ == '__main__':
+    import random
+
+    arr = array('i', [random.randint(0, 1000) for _ in range(100)])
+    print sort(arr)
