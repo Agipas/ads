574ff3daf9da3721cd9ebf2a85803f372bfad5be
vwvolodya_asus <vwvolodya@gmail.com>
2015-11-04 18:26:33 +0200

    fixed task 3

--- lngpok/lngpok.py
+++ lngpok/lngpok.py
@@ -51,19 +51,40 @@ def enhanced_searcher(array):
     jokers = sum((1 for el in array if el == 0))
     array = array[jokers:]
     results = [jokers]
+    if not array:
+        return jokers
+    if len(array) == 1:
+        return jokers + 1
 
     j = 0
-    jokers_left = jokers
     max_len = len(array) - 1
     while j < max_len:
-        i = 0
-        count = 0
-        while i < max_len - 1:
-            diff = array[i + 1] - array[i]
-
+        i = j
+        count = 1
+        jokers_left = jokers
+        while i < max_len:
+            diff = array[i + 1] - array[i] - 1
+            if diff == 0:
+                count += 1
+            elif diff == -1:
+                i += 1
+                count += jokers_left
+                jokers_left = 0
+                break
+            elif diff > jokers_left:
+                count += jokers_left
+                jokers_left = 0
+                break
+            else:
+                jokers_left -= diff
+                count += diff + 1
             i += 1
-
-        j += 1 #TODO change
+        if diff > jokers:   # there is no bug since array here is at least 2 elements long
+            j = i + 1
+        else:
+            j = i
+        if jokers_left:
+            count += jokers_left
         results.append(count)
 
     return max(results)
@@ -71,5 +92,5 @@ def enhanced_searcher(array):
 
 if __name__ == '__main__':
     arr = read_value('lngpok.in')
-    res = searcher(arr)
+    res = enhanced_searcher(arr)
     write_value('lngpok.out', res)
