1513895e44d801bb2ba3e33d463e69407fa46d70
vwvolodya <vwvolodya@gmail.com>
2015-11-04 16:57:44 +0200

    draft fix

--- lngpok/lngpok.py
+++ lngpok/lngpok.py
@@ -1,21 +1,6 @@
 from bisect import insort
 
 
-def read_value(path):
-    with open(path) as f:
-        data = f.readline()
-    ar = []
-    for el in data.split():
-        insort(ar, int(el))
-    print ar
-    return ar
-
-
-def write_value(path, result):
-    with open(path, 'w') as f:
-        f.write(str(result))
-
-
 def searcher(sorted_array):
     jokers = sum((1 for el in sorted_array if el == 0))
     sorted_array = sorted_array[jokers:]
@@ -45,6 +30,43 @@ def searcher(sorted_array):
     if results:
         count = max(results)
     return count + jokers_left
+
+
+def read_value(path):
+    with open(path) as f:
+        data = f.readline()
+    ar = []
+    for el in data.split():
+        insort(ar, int(el))
+    print ar
+    return ar
+
+
+def write_value(path, result):
+    with open(path, 'w') as f:
+        f.write(str(result))
+
+
+def enhanced_searcher(array):
+    jokers = sum((1 for el in array if el == 0))
+    array = array[jokers:]
+    results = [jokers]
+
+    j = 0
+    jokers_left = jokers
+    max_len = len(array) - 1
+    while j < max_len:
+        i = 0
+        count = 0
+        while i < max_len - 1:
+            diff = array[i + 1] - array[i]
+
+            i += 1
+
+        j += 1 #TODO change
+        results.append(count)
+
+    return max(results)
 
 
 if __name__ == '__main__':
