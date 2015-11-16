5470eea5580fe999362e75ca3c22467fbb7ab438
Volodymyr Holubets <vwvolodya@gmail.com>
2015-11-16 23:40:37 +0200

    another try for bugtrk

--- bugtrk/bugtrk.py
+++ bugtrk/bugtrk.py
@@ -69,7 +69,7 @@ def solver_3(n, h, w):
 def main():
     try:
         n, h, w = read_values(PROGRAM_NAME + '.in')
-        res = solver(n, h, w)
+        res = solver_2(n, h, w)
         write_result(PROGRAM_NAME + '.out', '{0}'.format(int(res)))
         print "Got production file"
         return
