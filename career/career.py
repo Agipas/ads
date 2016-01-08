fd07bacf152161438e6544a74d95308ded1c0a6f
vwvolodya_asus <vwvolodya@gmail.com>
2016-01-08 16:12:13 +0200

    try for dynamic programming

--- career/career.py
+++ career/career.py
@@ -1,10 +1,6 @@
-import heapq
-import string
-import random
 import time
 import os
 from pprint import pprint
-from collections import OrderedDict
 
 
 PROGRAM_NAME = 'career'
@@ -31,15 +27,31 @@ def write_result(path, result):
             f.write(str(result))
 
 
+def read_values(path):
+    result = list()
+    solutions = list()
+    with open(path) as f:
+        levels = int(f.readline())
+        for _ in xrange(levels):
+            data = f.readline().strip().split()
+            result.append([int(i)for i in data])
+    for i in xrange(levels+1):
+        solutions.append([0 for _ in xrange(levels + 1)])
+    return result, solutions, levels
+
+
 def compute(path):
-    graph, bottom_vertices = Graph.from_file(path)
-    start = graph.vertices[0]
-    # primive greedy algo
-    distances, path_predecessors = Graph.bellman_ford(graph, start)
-    last = distances.values()
-    if bottom_vertices:
-        last = distances.values()[-len(bottom_vertices):]
-    return max(last) + start.weight
+    data, solutions, levels = read_values(path)
+    for j in xrange(1, levels + 1):
+        for i in xrange(levels):
+            s1 = solutions[j][i - 1]
+            d1 = data[i - 1][j]
+            case_1 = s1 + d1
+            s2 = solutions[j + 1][i - 1]
+            d2 = data[i - 1][j]
+            case_2 = s2 + d2
+            solutions[j][i] = max(case_1, case_2)
+    return solutions[levels][0]
 
 
 def main():
@@ -57,177 +69,6 @@ def main():
             input_path = os.path.abspath(os.path.join(path, _file))
             res = compute(input_path)
             pprint(res)
-
-
-class NotDirectedAcyclicGraphError(ValueError):
-    pass
-
-
-class Vertex:
-    def __init__(self, weight, label=None):
-        self.label = label or ''.join([random.choice(string.ascii_uppercase) for _ in xrange(8)])
-        self.outbound_edges = []
-        self.weight = weight
-        self.inbound_edges = []
-
-    def __str__(self):
-        return str(self.label)
-
-    def __repr__(self):
-        return "Label: %s  " % self.label
-
-    def __hash__(self):
-        return hash(self.label)
-
-    def __eq__(self, other):
-        return self.label == other.label
-
-    def __add__(self, other):
-        return self.label + other.label
-
-
-class Edge:
-    def __init__(self, start_vertex, end_vertex, weight=None):
-        self.start_vertex = start_vertex
-        self.end_vertex = end_vertex
-        self.weight = weight
-
-    def __str__(self):
-        return "%s -> %s" % (self.start_vertex.label, self.end_vertex.label)
-
-    def __hash__(self):
-        return hash(self.start_vertex + self.end_vertex)
-
-
-class Graph:
-    def __init__(self, vertices, edges):
-        self.vertices = vertices
-        self.edges = edges
-
-    def __str__(self):
-        res = ''
-        for el in self.edges:
-            res += str(el) + '\n'
-        return res
-
-    @classmethod
-    def from_file(cls, path):
-        with open(path, "r") as input_file:
-            # The first two lines define the vertex count and the edge count.
-            levels = int(input_file.readline())
-            vertices = []
-            edges = []
-            top = [Vertex(int(input_file.readline()))]
-            vertices.extend(top)
-            pointer = top
-            next_vertices = None
-            for level in xrange(levels - 1):
-                weights = input_file.readline().split()
-                next_vertices = [Vertex(int(k)) for k in weights]
-                for index in xrange(len(pointer)):
-                    den1 = next_vertices[index]
-                    den2 = next_vertices[index + 1]
-                    one = Edge(pointer[index], den1, den1.weight)
-                    two = Edge(pointer[index], den2, den2.weight)
-                    pointer[index].outbound_edges = [one, two]
-                    den1.inbound_edges.append(one)
-                    den2.inbound_edges.append(two)
-                    edges.append(one)
-                    edges.append(two)
-                pointer = next_vertices
-                vertices.extend(next_vertices)
-
-        return cls(vertices, edges), next_vertices
-
-    @classmethod
-    def dijkstra(cls, graph, start_vertex):
-        # Initialization: setting all known shortest distances to infinity,
-        # and the start vertex will have the shortest distance to itself equal to 0.
-        INFINITY = -float('inf')
-        distances = OrderedDict((vertex.label, INFINITY) for vertex in graph.vertices)
-        distances[start_vertex.label] = 0
-
-        path_predecessors = {start_vertex.label: None}
-
-        # The heap will allow us to quickly pick an unvisited vertex with the least known distance.
-        # The implementation of heapq requires us to store (key, value) tuples, so we'll store (distance, vertex) tuples.
-        heap = []
-        heapq.heappush(heap, (0, start_vertex))
-
-        while len(heap) > 0:
-            # Picking the vertex with the smallest known distance so far.
-            distance, shortest_distance_vertex = heapq.heappop(heap)
-
-            # For each adjacent vertex v, check if the path from the current vertex would be more efficient
-            # than the one we've known before. I.e., if distance[current] + weight(current->v) < distance[v].
-            for edge in shortest_distance_vertex.outbound_edges:
-                neighbor_vertex = edge.end_vertex
-                alternative_distance = distances[shortest_distance_vertex.label] + edge.weight
-
-                # If we have indeed found a better path, remembering the new distance and predecessor.
-                if alternative_distance > distances[neighbor_vertex.label]:
-                    distances[neighbor_vertex.label] = alternative_distance
-                    path_predecessors[neighbor_vertex.label] = shortest_distance_vertex
-
-                    # Pushing the new distance to the heap.
-                    heapq.heappush(heap, (alternative_distance, neighbor_vertex))
-
-        return distances, path_predecessors
-
-    @classmethod
-    def reconstruct_shortest_path(cls, predecessors, start_vertex, end_vertex):
-        path = [end_vertex]
-        predecessor = predecessors[end_vertex]
-
-        while predecessor is not None:
-            path.insert(0, predecessor)
-            predecessor = predecessors[predecessor]
-
-        return path
-
-    @classmethod
-    def bellman_ford(cls, graph, start_vertex):
-        # Initialization of sub-problems:
-        # For the start vertex, the shortest path with edge budget = 0 will be 0.
-        # Other vertices will be unreachable given an edge budget of 0.
-        INFINITY = -float('inf')
-        distances = OrderedDict((vertex.label, INFINITY) for vertex in graph.vertices)
-        distances[start_vertex.label] = 0
-        predecessors = OrderedDict((vertex.label, None) for vertex in graph.vertices)
-
-        for edge_budget in range(1, len(graph.vertices) + 1):
-            # For detecting negative cycles, we'll remember if Case 2 wins at least for one vertex.
-            at_least_one_path_relaxed = False
-
-            for vertex in graph.vertices:
-                # Case 1: follow the path known before.
-                case1_distance = distances[vertex.label]
-                # Case 2: try reaching the current vertex through its inbound neighbors that are reachable in 'edge_budget - 1' steps.
-                case2_distance = INFINITY
-                better_predecessor = None
-
-                # This code could be more compact if we didn't want to return the actual paths in addition to distances.
-                # Otherwise, we have to remember the inbound vertex that led us to the relaxed path.
-                for inbound_edge in vertex.inbound_edges:
-                    candidate_distance = distances[inbound_edge.start_vertex.label] + inbound_edge.weight
-                    if candidate_distance > case2_distance:
-                        case2_distance = candidate_distance
-                        better_predecessor = inbound_edge.start_vertex
-
-                if case2_distance > case1_distance:
-                    at_least_one_path_relaxed = True
-                    predecessors[vertex.label] = better_predecessor
-
-                distances[vertex.label] = max(case1_distance, case2_distance)
-
-            # If we don't have negative-cost cycles, nothing should change if we run one extra iteration of Bellman-Ford.
-            # If some path has been relaxed even further, it means we've got a negative cycle and should raise an error.
-            if edge_budget == len(graph.vertices) and at_least_one_path_relaxed:
-                raise
-
-        #shortest_path_distances = [distances[vertex.label] for vertex in graph.vertices]
-        return distances, predecessors
-
 
 
 if __name__ == '__main__':
